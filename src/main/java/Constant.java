c465f180be5f11e6d73dee4c222123365fceb716
Sertac Eksi <sertac.eksi@ericsson.com>
2019-04-21 16:52:00 +0300

    Constant is added.

---
+++ src/main/java/Constant.java
@@ -0,0 +1,17 @@
+class Constant {
+    final static Integer PORT = 4445;
+    final static Integer CLIENT_INDEX = 0;
+    final static Integer SEQUENCE_INDEX = 4;
+    final static Integer VALUE_INDEX = 8;
+    final static Integer CHECKSUM_INDEX = 12;
+    final static Integer CLIENT_SIZE = 4;
+    final static Integer SEQUENCE_SIZE = 4;
+    final static Integer VALUE_SIZE = 4;
+    final static Integer NOTIFICATION_SIZE = 1;
+    final static Integer NOTIFY_MESSAGE_SIZE = 9;
+    final static Integer MESSAGE_SIZE = 20;
+    final static byte ACK_VALUE = 1;
+    final static byte NACK_VALUE = 1;
+    final static Integer MAX_TRY = 3;
+    final static String VALUES_FILE_EXTENSION = ".values.txt";
+}
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -13,12 +13,12 @@ import java.util.zip.CRC32;
 public class Server extends Thread {
     private DatagramSocket socket;
     private boolean running;
-    private byte[] receiverBuf = new byte[20];
+    private byte[] receiverBuf = new byte[Constant.MESSAGE_SIZE];
     private int lastSequence = 0;
     private Map<Integer, Integer> nackCount;
 
-    public Server() throws SocketException {
-        socket = new DatagramSocket(4445);
+    Server() throws SocketException {
+        socket = new DatagramSocket(Constant.PORT);
         nackCount = new HashMap<Integer, Integer>();
     }
 
@@ -30,23 +30,22 @@ public class Server extends Thread {
                 DatagramPacket packet = new DatagramPacket(receiverBuf, receiverBuf.length);
                 socket.receive(packet);
                 ByteBuffer wrapped = ByteBuffer.wrap(packet.getData());
-                Integer clientId = wrapped.getInt(0);
-                Integer sequenceId = wrapped.getInt(4);
-                Integer value = wrapped.getInt(8);
+                Integer clientId = wrapped.getInt(Constant.CLIENT_INDEX);
+                Integer sequenceId = wrapped.getInt(Constant.SEQUENCE_INDEX);
+                Integer value = wrapped.getInt(Constant.VALUE_INDEX);
                 ByteBuffer byteBuffer = null;
                 InetAddress address = packet.getAddress();
                 int port = packet.getPort();
                 if (lastSequence == 0 || lastSequence + 1 == sequenceId) {
                     if (isCheckSumTrue(packet.getData())) {
-                        byteBuffer = generateServerResponsePacket(clientId, sequenceId, (byte) 1);
+                        byteBuffer = generateServerResponsePacket(clientId, sequenceId, Constant.ACK_VALUE);
                         createValuesFile(clientId, sequenceId, value);
                     } else if (generateNackRecordOnServer(sequenceId)) {
-                        byteBuffer = generateServerResponsePacket(clientId, sequenceId, (byte) 0);
+                        byteBuffer = generateServerResponsePacket(clientId, sequenceId, Constant.NACK_VALUE);
                     }
                 } else if (generateNackRecordOnServer(sequenceId)) {
-                    byteBuffer = generateServerResponsePacket(clientId, lastSequence + 1, (byte) 0);
+                    byteBuffer = generateServerResponsePacket(clientId, lastSequence + 1, Constant.NACK_VALUE);
                 }
-
                 if (byteBuffer != null) {
                     senderBuf = byteBuffer.array();
                     packet = new DatagramPacket(senderBuf, senderBuf.length, address, port);
@@ -66,7 +65,7 @@ public class Server extends Thread {
         if (!nackCount.containsKey(sequenceId)) {
             nackCount.put(sequenceId, 1);
             generateNackRecord = true;
-        } else if (nackCount.get(sequenceId) < 3) {
+        } else if (nackCount.get(sequenceId) < Constant.MAX_TRY) {
             int temp = nackCount.get(sequenceId);
             temp++;
             nackCount.put(sequenceId, temp);
@@ -76,7 +75,7 @@ public class Server extends Thread {
     }
 
     private void createValuesFile(Integer clientId, Integer sequenceId, Integer value) throws IOException {
-        String fileName = clientId.toString() + ".values.txt";
+        String fileName = clientId.toString() + Constant.VALUES_FILE_EXTENSION;
         FileWriter fileWriter;
         if (lastSequence == 0) {
             fileWriter = new FileWriter(fileName);
@@ -95,23 +94,23 @@ public class Server extends Thread {
     private boolean isCheckSumTrue(byte[] dataArray) {
         ByteBuffer wrapped = ByteBuffer.wrap(dataArray);
         CRC32 checksum = new CRC32();
-        byte[] clientIdBytes = ByteBuffer.allocate(4).putInt(wrapped.getInt(0)).array();
-        byte[] sequenceIdBytes = ByteBuffer.allocate(4).putInt(wrapped.getInt(4)).array();
-        byte[] valueBytes = ByteBuffer.allocate(4).putInt(wrapped.getInt(8)).array();
+        byte[] clientIdBytes = ByteBuffer.allocate(Constant.CLIENT_SIZE).putInt(wrapped.getInt(Constant.CLIENT_INDEX)).array();
+        byte[] sequenceIdBytes = ByteBuffer.allocate(Constant.SEQUENCE_SIZE).putInt(wrapped.getInt(Constant.SEQUENCE_INDEX)).array();
+        byte[] valueBytes = ByteBuffer.allocate(Constant.VALUE_SIZE).putInt(wrapped.getInt(Constant.VALUE_INDEX)).array();
         checksum.update(clientIdBytes);
         checksum.update(sequenceIdBytes);
         checksum.update(valueBytes);
-        return checksum.getValue() == wrapped.getLong(12);
+        return checksum.getValue() == wrapped.getLong(Constant.CHECKSUM_INDEX);
     }
 
     private ByteBuffer generateServerResponsePacket(Integer clientId, Integer sequenceId, byte ack) {
-        byte[] clientIdBytes = ByteBuffer.allocate(4).putInt(clientId).array();
-        byte[] sequenceIdBytes = ByteBuffer.allocate(4).putInt(sequenceId).array();
-        ByteBuffer byteBuffer = ByteBuffer.allocate(9);
+        byte[] clientIdBytes = ByteBuffer.allocate(Constant.CLIENT_SIZE).putInt(clientId).array();
+        byte[] sequenceIdBytes = ByteBuffer.allocate(Constant.SEQUENCE_SIZE).putInt(sequenceId).array();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(Constant.NOTIFY_MESSAGE_SIZE);
         byteBuffer.put(clientIdBytes);
         byteBuffer.put(sequenceIdBytes);
         byte[] ackNumBytes;
-        ackNumBytes = ByteBuffer.allocate(1).put(ack).array();
+        ackNumBytes = ByteBuffer.allocate(Constant.NOTIFICATION_SIZE).put(ack).array();
         byteBuffer.put(ackNumBytes);
         return byteBuffer;
     }
